29a03aefb216c4c63f5ca79af1cb1a7a4029928f
Tulendra Chandrakar <98647158+Tulendra1111@users.noreply.github.com>
2025-09-22 00:45:36 +0530

    Update constants.js

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const Base_url = "https://edvironassignment.onrender.com" 
+export const Base_url = "https://edviron-assignment-schoolpayment-1.onrender.com" 
 // export const Base_url = "http://localhost:8080";
